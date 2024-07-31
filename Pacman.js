f364059c71d38b8f464992ae0645a3cf0318f719
qGuga <138495900+qGuga@users.noreply.github.com>
2024-07-31 01:34:28 -0300

    Pacman.js

---
+++ Pacman.js
@@ -0,0 +1,10 @@
+class PacMan extends Entidade {
+  constructor() {
+    super(TAMANHO_TELA, TAMANHO_TELA, VELOCIDADE_JOGADOR);
+  }
+
+  desenhar() {
+    fill('yellow');
+    arc(this.x + TAMANHO_TELA / 2, this.y + TAMANHO_TELA / 2, TAMANHO_TELA, TAMANHO_TELA, QUARTER_PI, -QUARTER_PI, PIE);
+  }
+}
