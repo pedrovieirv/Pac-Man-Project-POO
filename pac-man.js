3e7457eca34a94f4a4fbf7864925a0a1bb612a2b
pedrovieirv <pv417868@gmail.com>
2024-07-31 00:08:52 -0300

    commit de pedro

---
+++ pac-man.js
@@ -0,0 +1,57 @@
+const TILE_SIZE = 20;
+const ROWS = 11;
+const COLS = 20;
+const PLAYER_SPEED = 0.25 * TILE_SIZE; 
+const GHOST_SPEED = 0.25 * TILE_SIZE; 
+
+function generateMaze(rows, cols) {
+  let maze = [];
+  for (let row = 0; row < rows; row++) {
+    let rowArray = [];
+    for (let col = 0; col < cols; col++) {
+      if (row === 0 || row === rows - 1 || col === 0 || col === cols - 1) {
+        rowArray.push(1); 
+      } else {
+        rowArray.push(random(1) < 0.7 ? 0 : 1); 
+      }
+    }
+    maze.push(rowArray);
+  }
+
+  
+  maze[1][1] = 0;
+  maze[rows - 2][cols - 2] = 0;
+  return maze;
+}
+
+class PacMan {
+  constructor() {
+    this.x = TILE_SIZE;
+    this.y = TILE_SIZE;
+    this.velX = 0;
+    this.velY = 0;
+  }
+
+  move() {
+    if (this.canMove(this.x + this.velX, this.y + this.velY)) {
+      this.x += this.velX;
+      this.y += this.velY;
+    }
+  }
+
+  canMove(x, y) {
+    let col = floor(x / TILE_SIZE);
+    let row = floor(y / TILE_SIZE);
+    return maze[row][col] === 0;
+  }
+
+  setDirection(x, y) {
+    this.velX = x;
+    this.velY = y;
+  }
+
+  draw() {
+    fill('yellow');
+    circle(this.x + TILE_SIZE / 2, this.y + TILE_SIZE / 2, TILE_SIZE);
+  }
+}
