78bce771aedfa48f7bfdc185ed9b25c56674216e
qGuga <138495900+qGuga@users.noreply.github.com>
2024-09-19 16:41:38 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
+
 const TAMANHO_TELA = 20;
 const LINHAS = 11;
 const COLUNAS = 20;
 const VELOCIDADE_JOGADOR = 0.1 * TAMANHO_TELA;
 const VELOCIDADE_FANTASMA = 0.1 * TAMANHO_TELA;
+
 
 let labirinto;
 let mundo;
@@ -16,9 +18,17 @@ function setup() {
 
 function draw() {
   background(0);
-  mundo.atualizar();
-  mundo.desenhar();
+  if (!mundo.gameOver) {
+    mundo.atualizar();
+    mundo.desenhar();
+  } else {
+    fill('white');
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text('Game Over', width / 2, height / 2);
+  }
 }
+
 
 function gerarLabirinto(linhas, colunas) {
   const labirinto = [];
@@ -33,8 +43,8 @@ function gerarLabirinto(linhas, colunas) {
     }
     labirinto.push(linhaArray);
   }
-  labirinto[1][1] = 0;
-  labirinto[linhas - 2][colunas - 2] = 0;
+  labirinto[1][1] = 0; 
+  labirinto[linhas - 2][colunas - 2] = 0; 
   return labirinto;
 }
 
@@ -42,14 +52,15 @@ function eBorda(linha, coluna, linhas, colunas) {
   return linha === 0 || linha === linhas - 1 || coluna === 0 || coluna === colunas - 1;
 }
 
+
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
-    mundo.pacman.definirDirecao(-VELOCIDADE_JOGADOR, 0);
+    mundo.pacman.definirDirecao(-mundo.pacman.velocidade, 0);
   } else if (keyCode === RIGHT_ARROW) {
-    mundo.pacman.definirDirecao(VELOCIDADE_JOGADOR, 0);
+    mundo.pacman.definirDirecao(mundo.pacman.velocidade, 0);
   } else if (keyCode === UP_ARROW) {
-    mundo.pacman.definirDirecao(0, -VELOCIDADE_JOGADOR);
+    mundo.pacman.definirDirecao(0, -mundo.pacman.velocidade);
   } else if (keyCode === DOWN_ARROW) {
-    mundo.pacman.definirDirecao(0, VELOCIDADE_JOGADOR);
+    mundo.pacman.definirDirecao(0, mundo.pacman.velocidade);
   }
 }
